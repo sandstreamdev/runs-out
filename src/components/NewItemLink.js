f61a5fd460b2ba7e4a86b5d83d5a9f5c3579d62d
Marek Rozmus <marek.rozmus@sandstream.pl>
2019-08-16 23:24:57 +0200

    Add new item link component and few fixes

---
+++ src/components/NewItemLink.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+
+import './NewItemLink.css';
+
+class NewItemLink extends PureComponent {
+  render() {
+    const { label, to } = this.props;
+
+    return (
+      <Link className="new-item-link" to={to}>
+        <div className="fake-input">
+          <span className="placeholder">{label}</span>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default NewItemLink;
