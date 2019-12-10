70a59a19c4e9cd4aad932f72c6b72c231fb03bff
Marek Rozmus <marek.rozmus@sandstream.pl>
2019-12-10 11:34:15 +0100

    Update icon and dependency import

--- src/components/temp_data/entries.js
+++ src/components/temp_data/entries.js
@@ -1,15 +1,15 @@
 export const entries = [
   {
     id: 1,
-    name: 'Coffee',
-    description: 'Item description that can be only one line height...',
+    name: "Coffee",
+    description: "Item description that can be only one line height...",
     commentsCount: 8,
-    likesCount: 2,
+    likesCount: 2
   },
   {
     id: 2,
-    name: 'Pizza',
-    description: 'Item description that can be only one line height...',
+    name: "Pizza",
+    description: "Item description that can be only one line height...",
     commentsCount: 8,
     likesCount: 13,
     notificationsCount: 2,
@@ -17,29 +17,30 @@ export const entries = [
   },
   {
     id: 3,
-    name: 'Black Tee (Lipton)',
+    name: "Black Tee (Lipton)",
     commentsCount: 12,
     likesCount: 2,
     isDone: true
   },
   {
     id: 4,
-    name: 'Computer',
-    description: 'Better, faster, bigger',
+    name: "Computer",
+    description: "Better, faster, bigger",
     commentsCount: 1,
     likesCount: 2
   },
   {
     id: 5,
-    name: 'Paper towels',
-    description: 'Nice and soft please :)',
+    name: "Paper towels",
+    description: "Nice and soft please :)",
     commentsCount: 8,
-    likesCount: 33
+    likesCount: 33,
+    isVoted: true
   },
   {
     id: 6,
-    name: 'Water filters',
-    description: '30 days variant',
+    name: "Water filters",
+    description: "30 days variant",
     commentsCount: 1,
     likesCount: 2
   }
